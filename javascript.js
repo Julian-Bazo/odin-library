27d82471be6d6e25425f6759991ca4d335789105
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-07-13 10:32:58 -0400

    Add eventlistener to new book button to show form
    Adds an evenlistener to the add new book button that will toggle the displaying of the form. Added some styles to the form as well.

--- javascript.js
+++ javascript.js
@@ -35,12 +35,28 @@ const mappedLibrary = myLibrary.map((book) => {
     const bookCard = document.createElement("div");
     bookCard.classList.add("book");
     mainGrid.appendChild(bookCard);
-    bookCard.innerText = `Title: ${book.title} \n Author: ${book.author} \n Pages: ${book.pages}`;
+    bookCard.innerText = `Title: ${book.title} \n Author: ${book.author} \n Pages: ${book.pages} \n ID: ${book.bookID}`;
 })
 
 const addButton = document.querySelector(".add-button");
-// const stableForm = document.querySelector(".stable-form-div");
+const formDiv = document.querySelector(".form-div");
+
+let formHider = false;
 
 addButton.addEventListener("click", () => {
-
+    if (formHider === false) {
+    formDiv.style.display = "block";
+    formHider = true;
+    }
+    else {
+        formDiv.style.display = "none";
+        formHider = false;
+    }
 })
+
+// TO
+// Fix ID system
+// Disable default behavior on submit button
+// Add new book to grid when submit button is pressed
+// Clear form with a "false click" on reset button when submitted
+// Set form display to none on submit as well
